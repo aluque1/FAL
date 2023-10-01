7feb89d7d2f2beed99ccae278e32fcaaae9f1f24
Aluque <32274359+aluque1@users.noreply.github.com>
2023-10-01 13:34:09 +0200

    intentando hacer entrando al cine

--- problemas_iterativos/entrando_al_cine.cpp
+++ problemas_iterativos/entrando_al_cine.cpp
@@ -8,20 +8,19 @@ using namespace std;
 int v[10000];
 
 // { Pre : 0 <= n <= longitud(v) }
-bool resuelve() /* return ret */
+void resuelve() /* return ret */
 {
     int num_personas;
     int i = 0;
     cin >> num_personas;
-    bool tramo_derecho;
-
-    while (i < num_personas)
-    {
-        cin >> v[i];
-    }
-    
 }
-// { Post : ret = bool, i ; 0 <= i <= n : }
+// { Post: ret = Ex. k : (0 <= k < n)
+//                     : todosPares(v, 0, k) ^
+//                       todosImpares(v, k, n)
+// donde : 
+//      todosPares(v, a, b) = P.t i : a <= i < b : v[i] mod 2 = 0 <-----+ nos fijamos que a esta dentro de v[i] mienstras b no lo esta
+//      todosImpares(v, a, b) = P.t i : a <= i < b : v[i] mod 2 = 1 <---+   
+// }
 
 int main(int argc, char const *argv[])
 {
