90a7a8f22c70ff9d9901c3c51c41714c4d3efd9c
Aluque <32274359+aluque1@users.noreply.github.com>
2023-10-21 19:55:09 +0200

    problema 140 suma digitos acepta el reto

---
+++ problemas_recursivos/suma_digitos.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+// problema 140 Suma de dijitos
+// resuelto en la funcion suma(int n)
+
+int suma(int n)
+{
+    int sum = 0;
+    
+    if (n < 10) // caso base
+    {
+        sum += n;
+    }
+    else // caso recursivo
+    {
+        sum += n % 10 + suma(n / 10);
+        cout << " + ";
+    }
+    
+    cout << n % 10;
+    return sum;
+}
+
+int main(int argc, char const *argv[])
+{
+    int n;
+    cin >> n;
+    while (n > -1)
+    {
+        int sum = suma(n);
+        cout << " = " << sum << endl;
+        cin >> n;
+    }
+
+    return 0;
+}
